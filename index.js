ca4ca2657f964930e57aaa9bb0fd1dfb46acf16c
dgbkn <anandrambkn@gmail.com>
2022-03-24 18:32:25 +0530

    asdad

--- index.js
+++ index.js
@@ -252,7 +252,23 @@ app.get("/getByRangeDataSheet1PWnew", async (req, res) => {
 // });
 
 
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+  });
+  
+  // error handler middleware
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500).send({
+      error: {
+        status: error.status || 500,
+        message: error.message || "Internal Server Error",
+      },
+    });
+  });
 
+  
 app.listen(process.env.PORT || 3000, () => {
 
     console.log(`server started on ${process.env.PORT || 3000}`)
