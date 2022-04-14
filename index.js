826501dcf2b5a73265c93e78a3b6fe20122bea8f
dgbkn <anandrambkn@gmail.com>
2022-04-14 15:08:15 +0530

    sfsdf

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 var bodyParser = require('body-parser');
 const formidable = require('express-formidable');
 var cors = require('cors')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 //googleapisa
 const { google } = require("googleapis");
@@ -45,7 +46,7 @@ const auth = new google.auth.GoogleAuth({
 // });
 
 app.get("/", function (req, res) {
-  res.send("Welcome 🤗😁");
+    res.send("Welcome 🤗😁");
 });
 
 app.get("/getAllBatches", async (req, res) => {
@@ -78,43 +79,43 @@ app.get("/getAllBatches", async (req, res) => {
             return true; // skip
         }
         return false;
-      })
-      .map((item) => { 
-        var finalObj = {};
+    })
+        .map((item) => {
+            var finalObj = {};
 
-        if (0 in item.values && "formattedValue" in item.values[0]) {
-            var lec = item.values[0];
-            finalObj.batchName = lec.formattedValue;
-        }
-    
-        if (1 in item.values) {
-            var subparts = item.values[1].formattedValue.split("-");
-            var subparts = subparts.filter((item)=>{
-                if(item == ""){
-                    return false;
-                }
-                return true;
-            });
-        }else{
-            var subparts = null;
-        }
+            if (0 in item.values && "formattedValue" in item.values[0]) {
+                var lec = item.values[0];
+                finalObj.batchName = lec.formattedValue;
+            }
 
-        if(subparts){
-            finalObj.subjects=[];
-            subparts.forEach((item)=>{
-            var d = item.split(",");
-            finalObj.subjects.push({"name":d[0],"range":d[1]});
-            });
-        }
-    
-    
-        return finalObj;
-    });
+            if (1 in item.values) {
+                var subparts = item.values[1].formattedValue.split("-");
+                var subparts = subparts.filter((item) => {
+                    if (item == "") {
+                        return false;
+                    }
+                    return true;
+                });
+            } else {
+                var subparts = null;
+            }
+
+            if (subparts) {
+                finalObj.subjects = [];
+                subparts.forEach((item) => {
+                    var d = item.split(",");
+                    finalObj.subjects.push({ "name": d[0], "range": d[1] });
+                });
+            }
 
 
+            return finalObj;
+        });
 
 
-    
+
+
+
     res.send(the_real_row_data);
 });
 
@@ -123,100 +124,145 @@ app.get("/getAllBatches", async (req, res) => {
 app.get("/getSubjectData", async (req, res) => {
     // console.log(req.body);
 
-    if(req.query.delay){
-    const sleep = async () => {
-        await new Promise(async (resolve) => {
-          setTimeout(() => resolve(), 15000);
-        });
-      };
+    if (req.query.delay) {
+        const sleep = async () => {
+            await new Promise(async (resolve) => {
+                setTimeout(() => resolve(), 15000);
+            });
+        };
 
-      await sleep();
-}
+        await sleep();
+    }
 
     const range = req.fields?.range || req.query?.range;
 
-    if(range){
-    var authobj = fs.readFileSync('token.json');
-    authobj = JSON.parse(authobj);
+    if (range) {
 
-    const authClientObject = Object.create(authobj);
-    // const authClientObject = await auth.getClient();
+        if (range.includes('yt^')) {
+             
+            var playlistData = range.split('^');
+            var index = 2 in playlistData ?  playlistData[2] : 0;
+            playlistId = playlistData[1];
+
+           var apiData = await fetch("https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyAa-o55aIMt4YC0mhPyp8WfGql5DVg_fp4&part=snippet&maxResults=50&playlistId="+playlistId, {
+                "headers": {
+                  "accept": "application/json, text/javascript, */*; q=0.01",
+                  "accept-language": "en-US,en-IN;q=0.9,en;q=0.8,hi-IN;q=0.7,hi;q=0.6",
+                  "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100\"",
+                  "sec-ch-ua-mobile": "?0",
+                  "sec-ch-ua-platform": "\"Windows\"",
+                  "sec-fetch-dest": "empty",
+                  "sec-fetch-mode": "cors",
+                  "sec-fetch-site": "cross-site",
+                },
+                "referrer": "https://mattw.io/",
+                "referrerPolicy": "strict-origin-when-cross-origin",
+                "body": null,
+                "method": "GET",
+                "mode": "cors",
+                "credentials": "omit"
+              });
+              
+              apiData = await apiData.json();
+
+              apiData = apiData.items.map((e)=>{
+                  var finadItem = e.snippet;
+                  var thumbs = finadItem.thumbnails;
+                  return {
+                      title:finadItem.title,
+                      thumb:thumbs[Object.keys(thumbs)[Object.keys(thumbs).length - 1]].url,
+                      videoId:finadItem.resourceId.videoId,
+                  };
+              });
+
+              apiData = JSON.stringify(apiData);
+              
+              res.send(apiData);
+
+            }else{
+
+        var authobj = fs.readFileSync('token.json');
+        authobj = JSON.parse(authobj);
+
+        const authClientObject = Object.create(authobj);
+        // const authClientObject = await auth.getClient();
 
 
-    //Google sheets instance
-    const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
+        //Google sheets instance
+        const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
 
 
-    var readData = await googleSheetsInstance.spreadsheets.get({
-        auth, //auth object
-        spreadsheetId: "1rwkKRFMCl2-iZxnJg1yY7NcEa4z_HtGOEFuByTOZ5Lg", // spreadsheet id
-        ranges: [range], //range of cells to read from.,
-        includeGridData: true
-    })
+        var readData = await googleSheetsInstance.spreadsheets.get({
+            auth, //auth object
+            spreadsheetId: "1rwkKRFMCl2-iZxnJg1yY7NcEa4z_HtGOEFuByTOZ5Lg", // spreadsheet id
+            ranges: [range], //range of cells to read from.,
+            includeGridData: true
+        })
 
 
-    readData = readData.data.sheets[0].data;
-    var rowData = readData[0].rowData;
+        readData = readData.data.sheets[0].data;
+        var rowData = readData[0].rowData;
 
-    var the_real_row_data = rowData.filter((item) => {
-        if ((0 in item.values && "formattedValue" in item.values[0]) && item.values[0].formattedValue != "Class Cancelled") {
-            return true; // skip
-        }
-        return false;
-      }).map((item) => {
-
-        // return item.values?.map((singleitem) => {
-
-        //     if(singleitem.formattedValue == "NOTES" || singleitem.formattedValue == "DPP"){
-        //      var ret = {"type": singleitem.formattedValue, "link" : singleitem.hyperlink}
-        //     }else{
-        //         var ret = {"name": singleitem.formattedValue,"type":"LECTURE", "link" : singleitem.hyperlink};
-        //     }
-
-        //     return ret;
-        // });
-
-
-
-        var finalObj = {};
-
-
-        if (0 in item.values && "formattedValue" in item.values[0]) {
-            var lec = item.values[0];
-            finalObj.name = lec.formattedValue;
-            finalObj.lecture = lec.hyperlink;
-        }
-        else{
+        var the_real_row_data = rowData.filter((item) => {
+            if ((0 in item.values && "formattedValue" in item.values[0]) && item.values[0].formattedValue != "Class Cancelled") {
+                return true; // skip
+            }
             return false;
-        }
+        }).map((item) => {
 
-        if (1 in item.values) {
-            var note = item.values[1];
-            finalObj.notes = note.hyperlink;
-        }else{
-            finalObj.notes = "";
-        }
+            // return item.values?.map((singleitem) => {
 
-        if (2 in item.values) {
-            var dpp = item.values[2];
-            finalObj.dpp = dpp.hyperlink;
-        }else{
-            finalObj.dpp = "";
-        }
+            //     if(singleitem.formattedValue == "NOTES" || singleitem.formattedValue == "DPP"){
+            //      var ret = {"type": singleitem.formattedValue, "link" : singleitem.hyperlink}
+            //     }else{
+            //         var ret = {"name": singleitem.formattedValue,"type":"LECTURE", "link" : singleitem.hyperlink};
+            //     }
 
-        return finalObj;
-    });
+            //     return ret;
+            // });
 
 
-    var finalData = the_real_row_data.map(item => {
-        if (item) {
-            return item;
-        }
-    });
 
-    res.send(JSON.stringify(finalData));
-    }else{
-    res.send('send range');
+            var finalObj = {};
+
+
+            if (0 in item.values && "formattedValue" in item.values[0]) {
+                var lec = item.values[0];
+                finalObj.name = lec.formattedValue;
+                finalObj.lecture = lec.hyperlink;
+            }
+            else {
+                return false;
+            }
+
+            if (1 in item.values) {
+                var note = item.values[1];
+                finalObj.notes = note.hyperlink;
+            } else {
+                finalObj.notes = "";
+            }
+
+            if (2 in item.values) {
+                var dpp = item.values[2];
+                finalObj.dpp = dpp.hyperlink;
+            } else {
+                finalObj.dpp = "";
+            }
+
+            return finalObj;
+        });
+
+
+        var finalData = the_real_row_data.map(item => {
+            if (item) {
+                return item;
+            }
+        });
+
+        res.send(JSON.stringify(finalData));
+    }
+    } else {
+        res.send('send range');
     }
 });
 
@@ -273,19 +319,19 @@ app.use((req, res, next) => {
     const error = new Error("Not found");
     error.status = 404;
     next(error);
-  });
-  
-  // error handler middleware
-  app.use((error, req, res, next) => {
-    res.status(error.status || 500).send({
-      error: {
-        status: error.status || 500,
-        message: error.message || "Internal Server Error",
-      },
-    });
-  });
+});
 
-  
+// error handler middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).send({
+        error: {
+            status: error.status || 500,
+            message: error.message || "Internal Server Error",
+        },
+    });
+});
+
+
 app.listen(process.env.PORT || 3000, () => {
 
     console.log(`server started on ${process.env.PORT || 3000}`)
